mhajas/keycloak

Leverage Infinispan lifespan for ExpirableEntities in HotRod storage

testsuite/model/src/test/java/org/keycloak/testsuite/model/KeycloakModelTest.java
@@ -16,6 +16,7 @@
  */
 package org.keycloak.testsuite.model;
 
+import org.infinispan.client.hotrod.RemoteCache;
 import org.junit.Assert;
 import org.keycloak.Config.Scope;
 import org.keycloak.authorization.AuthorizationSpi;
@@ -45,6 +46,8 @@ import org.keycloak.models.DeploymentStateSpi;
 import org.keycloak.models.UserLoginFailureSpi;
 import org.keycloak.models.UserSessionSpi;
 import org.keycloak.models.UserSpi;
+import org.keycloak.models.map.storage.hotRod.connections.DefaultHotRodConnectionProviderFactory;
+import org.keycloak.models.map.storage.hotRod.connections.HotRodConnectionProvider;
 import org.keycloak.models.locking.GlobalLockProviderSpi;
 import org.keycloak.models.utils.KeycloakModelUtils;
 import org.keycloak.models.utils.PostMigrationEvent;
@@ -63,7 +66,7 @@ import java.lang.management.LockInfo;
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadInfo;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -637,4 +640,25 @@ public abstract class KeycloakModelTest {
         return realm;
     }
 
+    /**
+     * Moves time on the Keycloak server as well as on the remote Infinispan server if the Infinispan is used.
+     * The caller of the method is responsible to reset time offset (by calling advance(0)) after a test run.
+     * @param seconds time offset in seconds by which Keycloak (and Infinispan) server time is moved
+     */
+    protected void advanceTime(int seconds) {
+        inComittedTransaction(session -> {
+            // move time on Hot Rod server if present
+            HotRodConnectionProvider hotRodConnectionProvider = session.getProvider(HotRodConnectionProvider.class);
+            if (hotRodConnectionProvider != null) {
+                RemoteCache<Object, Object> scriptCache = hotRodConnectionProvider.getRemoteCache(DefaultHotRodConnectionProviderFactory.SCRIPT_CACHE);
+                if (scriptCache != null) {
+                    Map<String, Object> param = new HashMap<>();
+                    param.put("timeService", seconds);
+                    Object returnFromTask = scriptCache.execute("InfinispanTimeServiceTask", param);
+                    LOG.info(returnFromTask);
+                }
+            }
+            Time.setOffset(seconds);
+        });
+    }
 }

sky-cloak/locke

testsuite/model/src/test/java/org/keycloak/testsuite/model/KeycloakModelTest.java
@@ -16,6 +16,7 @@
  */
 package org.keycloak.testsuite.model;
 
+import org.infinispan.client.hotrod.RemoteCache;
 import org.junit.Assert;
 import org.keycloak.Config.Scope;
 import org.keycloak.authorization.AuthorizationSpi;
@@ -45,6 +46,8 @@ import org.keycloak.models.DeploymentStateSpi;
 import org.keycloak.models.UserLoginFailureSpi;
 import org.keycloak.models.UserSessionSpi;
 import org.keycloak.models.UserSpi;
+import org.keycloak.models.map.storage.hotRod.connections.DefaultHotRodConnectionProviderFactory;
+import org.keycloak.models.map.storage.hotRod.connections.HotRodConnectionProvider;
 import org.keycloak.models.locking.GlobalLockProviderSpi;
 import org.keycloak.models.utils.KeycloakModelUtils;
 import org.keycloak.models.utils.PostMigrationEvent;
@@ -63,7 +66,7 @@ import java.lang.management.LockInfo;
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadInfo;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -81,7 +84,6 @@ import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
@@ -527,17 +529,17 @@ public abstract class KeycloakModelTest {
 
     @Before
     public final void createEnvironment() {
-        Time.setOffset(0);
+        setTimeOffset(0);
         USE_DEFAULT_FACTORY = isUseSameKeycloakSessionFactoryForAllThreads();
         KeycloakModelUtils.runJobInTransaction(getFactory(), this::createEnvironment);
     }
 
     @After
     public final void cleanEnvironment() {
-        Time.setOffset(0);
         if (getFactory() == null) {
             reinitializeKeycloakSessionFactory();
         }
+        setTimeOffset(0);
         KeycloakModelUtils.runJobInTransaction(getFactory(), this::cleanEnvironment);
     }
 
@@ -637,4 +639,24 @@ public abstract class KeycloakModelTest {
         return realm;
     }
 
+    /**
+     * Moves time on the Keycloak server as well as on the remote Infinispan server if the Infinispan is used.
+     * @param seconds time offset in seconds by which Keycloak (and Infinispan) server time is moved
+     */
+    protected void setTimeOffset(int seconds) {
+        inComittedTransaction(session -> {
+            // move time on Hot Rod server if present
+            HotRodConnectionProvider hotRodConnectionProvider = session.getProvider(HotRodConnectionProvider.class);
+            if (hotRodConnectionProvider != null) {
+                RemoteCache<Object, Object> scriptCache = hotRodConnectionProvider.getRemoteCache(DefaultHotRodConnectionProviderFactory.SCRIPT_CACHE);
+                if (scriptCache != null) {
+                    Map<String, Object> param = new HashMap<>();
+                    param.put("timeService", seconds);
+                    Object returnFromTask = scriptCache.execute("InfinispanTimeServiceTask", param);
+                    LOG.info(returnFromTask);
+                }
+            }
+            Time.setOffset(seconds);
+        });
+    }
 }