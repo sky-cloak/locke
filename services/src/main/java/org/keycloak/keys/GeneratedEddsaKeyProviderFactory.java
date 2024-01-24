rmartinc/keycloak

Supporting EdDSA
closes #15714

Co-authored-by: Muhammad Zakwan Bin Mohd Zahid <muhammadzakwan.mohdzahid.fg@hitachi.com>

services/src/main/java/org/keycloak/keys/GeneratedEddsaKeyProviderFactory.java
@@ -1,0 +1,141 @@
+/*
+ * Copyright 2023 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.keycloak.keys;
+
+import org.jboss.logging.Logger;
+import org.keycloak.common.util.Base64;
+import org.keycloak.common.util.MultivaluedHashMap;
+import org.keycloak.component.ComponentModel;
+import org.keycloak.component.ComponentValidationException;
+import org.keycloak.crypto.Algorithm;
+import org.keycloak.crypto.KeyUse;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.provider.ConfigurationValidationHelper;
+import org.keycloak.provider.ProviderConfigProperty;
+
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.interfaces.EdECPublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.List;
+
+/**
+ * @author <a href="mailto:takashi.norimatsu.ws@hitachi.com">Takashi Norimatsu</a>
+ */
+public class GeneratedEddsaKeyProviderFactory extends AbstractEddsaKeyProviderFactory {
+
+    private static final Logger logger = Logger.getLogger(GeneratedEddsaKeyProviderFactory.class);
+
+    public static final String ID = "eddsa-generated";
+
+    private static final String HELP_TEXT = "Generates EdDSA keys";
+
+    public static final String DEFAULT_EDDSA_ELLIPTIC_CURVE = Algorithm.Ed25519;
+
+    private static final List<ProviderConfigProperty> CONFIG_PROPERTIES = AbstractEddsaKeyProviderFactory.configurationBuilder()
+            .property(EDDSA_ELLIPTIC_CURVE_PROPERTY)
+            .build();
+
+    @Override
+    public KeyProvider create(KeycloakSession session, ComponentModel model) {
+        return new GeneratedEddsaKeyProvider(session.getContext().getRealm(), model);
+    }
+
+    @Override
+    public boolean createFallbackKeys(KeycloakSession session, KeyUse keyUse, String algorithm) {
+        if (keyUse.equals(KeyUse.SIG) && (algorithm.equals(Algorithm.Ed25519) || algorithm.equals(Algorithm.Ed448))) {
+            RealmModel realm = session.getContext().getRealm();
+
+            ComponentModel generated = new ComponentModel();
+            generated.setName("fallback-" + algorithm);
+            generated.setParentId(realm.getId());
+            generated.setProviderId(ID);
+            generated.setProviderType(KeyProvider.class.getName());
+
+            MultivaluedHashMap<String, String> config = new MultivaluedHashMap<>();
+            config.putSingle(Attributes.PRIORITY_KEY, "-100");
+            config.putSingle(EDDSA_ELLIPTIC_CURVE_KEY, algorithm);
+            generated.setConfig(config);
+
+            realm.addComponentModel(generated);
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String getHelpText() {
+        return HELP_TEXT;
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return CONFIG_PROPERTIES;
+    }
+
+    @Override
+    public String getId() {
+        return ID;
+    }
+
+    @Override
+    public void validateConfiguration(KeycloakSession session, RealmModel realm, ComponentModel model) throws ComponentValidationException {
+        super.validateConfiguration(session, realm, model);
+
+        ConfigurationValidationHelper.check(model).checkList(EDDSA_ELLIPTIC_CURVE_PROPERTY, false);
+
+        String curveName = model.get(EDDSA_ELLIPTIC_CURVE_KEY);
+        if (curveName == null) curveName = DEFAULT_EDDSA_ELLIPTIC_CURVE;
+
+        if (!(model.contains(EDDSA_PRIVATE_KEY_KEY) && model.contains(EDDSA_PUBLIC_KEY_KEY))) {
+            generateKeys(model, curveName);
+            logger.debugv("Generated keys for {0}", realm.getName());
+        } else {
+            String currentEdEc = getCurveFromPublicKey(model.getConfig().getFirst(GeneratedEddsaKeyProviderFactory.EDDSA_PUBLIC_KEY_KEY));
+            if (!curveName.equals(currentEdEc)) {
+                generateKeys(model, curveName);
+                logger.debugv("Twisted Edwards Curve changed, generating new keys for {0}", realm.getName());
+            }
+        }
+    }
+
+    private void generateKeys(ComponentModel model, String curveName) {
+        KeyPair keyPair;
+        try {
+            keyPair = generateEddsaKeyPair(curveName);
+            model.put(EDDSA_PRIVATE_KEY_KEY, Base64.encodeBytes(keyPair.getPrivate().getEncoded()));
+            model.put(EDDSA_PUBLIC_KEY_KEY, Base64.encodeBytes(keyPair.getPublic().getEncoded()));
+            model.put(EDDSA_ELLIPTIC_CURVE_KEY, curveName);
+        } catch (Throwable t) {
+            throw new ComponentValidationException("Failed to generate EdDSA keys", t);
+        }
+    }
+
+    private String getCurveFromPublicKey(String publicEddsaKeyBase64Encoded) {
+        try {
+            KeyFactory kf = KeyFactory.getInstance("EdDSA");
+            X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(Base64.decode(publicEddsaKeyBase64Encoded));
+            EdECPublicKey edEcKey = (EdECPublicKey) kf.generatePublic(publicKeySpec);
+            return edEcKey.getParams().getName();
+        } catch (Throwable t) {
+            throw new ComponentValidationException("Failed to get Twisted Edwards Curve from its public key", t);
+        }
+    }
+}

services/src/test/java/org/keycloak/jose/jwk/ServerJWKTest.java
@@ -1,0 +1,127 @@
+/*
+ * Copyright 2023 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.keycloak.jose.jwk;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.nio.charset.StandardCharsets;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.keycloak.common.util.KeyUtils;
+import org.keycloak.crypto.Algorithm;
+import org.keycloak.crypto.JavaAlgorithm;
+import org.keycloak.rule.CryptoInitRule;
+import org.keycloak.util.JsonSerialization;
+
+/**
+ * This is not tested in keycloak-core. The subclasses should be created in the crypto modules to make sure it is tested with corresponding modules (bouncycastle VS bouncycastle-fips)
+ *
+ * @author <a href="mailto:takashi.norimatsu.ws@hitachi.com">Takashi Norimatsu</a>
+ */
+public abstract class ServerJWKTest {
+
+    @ClassRule
+    public static CryptoInitRule cryptoInitRule = new CryptoInitRule();
+
+
+    @Test
+    public void publicEd25519() throws Exception {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance(Algorithm.Ed25519);
+        KeyPair keyPair = keyGen.generateKeyPair();
+
+        PublicKey publicKey = keyPair.getPublic();
+        JWK jwk = JWKBuilder.create().kid(KeyUtils.createKeyId(keyPair.getPublic())).algorithm(Algorithm.Ed25519).okp(publicKey);
+
+        assertEquals("OKP", jwk.getKeyType());
+        assertEquals("Ed25519", jwk.getAlgorithm());
+        assertEquals("sig", jwk.getPublicKeyUse());
+
+        assertTrue(jwk instanceof OKPPublicJWK);
+
+        OKPPublicJWK okpJwk = (OKPPublicJWK) jwk;
+
+        assertNotNull(okpJwk.getCrv());
+        assertNotNull(okpJwk.getX());
+
+        String jwkJson = JsonSerialization.writeValueAsString(jwk);
+
+        JWKParser parser = JWKParser.create().parse(jwkJson);
+        PublicKey publicKeyFromJwk = parser.toPublicKey();
+
+        assertArrayEquals(publicKey.getEncoded(), publicKeyFromJwk.getEncoded());
+
+        byte[] data = "Some test string".getBytes(StandardCharsets.UTF_8);
+        byte[] sign = sign(data, JavaAlgorithm.Ed25519, keyPair.getPrivate());
+        verify(data, sign, JavaAlgorithm.Ed25519, publicKeyFromJwk);
+    }
+
+    @Test
+    public void publicEd448() throws Exception {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance(Algorithm.Ed448);
+        KeyPair keyPair = keyGen.generateKeyPair();
+
+        PublicKey publicKey = keyPair.getPublic();
+        JWK jwk = JWKBuilder.create().kid(KeyUtils.createKeyId(keyPair.getPublic())).algorithm(Algorithm.Ed448).okp(publicKey);
+
+        assertEquals("OKP", jwk.getKeyType());
+        assertEquals("Ed448", jwk.getAlgorithm());
+        assertEquals("sig", jwk.getPublicKeyUse());
+
+        assertTrue(jwk instanceof OKPPublicJWK);
+
+        OKPPublicJWK okpJwk = (OKPPublicJWK) jwk;
+
+        assertNotNull(okpJwk.getCrv());
+        assertNotNull(okpJwk.getX());
+
+        String jwkJson = JsonSerialization.writeValueAsString(jwk);
+
+        JWKParser parser = JWKParser.create().parse(jwkJson);
+        PublicKey publicKeyFromJwk = parser.toPublicKey();
+
+        assertArrayEquals(publicKey.getEncoded(), publicKeyFromJwk.getEncoded());
+
+        byte[] data = "Some test string".getBytes(StandardCharsets.UTF_8);
+        byte[] sign = sign(data, JavaAlgorithm.Ed448, keyPair.getPrivate());
+        verify(data, sign, JavaAlgorithm.Ed448, publicKeyFromJwk);
+    }
+
+    private byte[] sign(byte[] data, String javaAlgorithm, PrivateKey key) throws Exception {
+        Signature signature = Signature.getInstance(javaAlgorithm);
+        signature.initSign(key);
+        signature.update(data);
+        return signature.sign();
+    }
+
+    private boolean verify(byte[] data, byte[] signature, String javaAlgorithm, PublicKey key) throws Exception {
+        Signature verifier = Signature.getInstance(javaAlgorithm);
+        verifier.initVerify(key);
+        verifier.update(data);
+        return verifier.verify(signature);
+    }
+
+}

sky-cloak/locke

Signed-off-by: Takashi Norimatsu <takashi.norimatsu.ws@hitachi.com>
Co-authored-by: rmartinc <rmartinc@redhat.com>

services/src/main/java/org/keycloak/keys/GeneratedEddsaKeyProviderFactory.java
@@ -1,0 +1,141 @@
+/*
+ * Copyright 2023 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.keycloak.keys;
+
+import org.jboss.logging.Logger;
+import org.keycloak.common.util.Base64;
+import org.keycloak.common.util.MultivaluedHashMap;
+import org.keycloak.component.ComponentModel;
+import org.keycloak.component.ComponentValidationException;
+import org.keycloak.crypto.Algorithm;
+import org.keycloak.crypto.KeyUse;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.provider.ConfigurationValidationHelper;
+import org.keycloak.provider.ProviderConfigProperty;
+
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.interfaces.EdECPublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.List;
+
+/**
+ * @author <a href="mailto:takashi.norimatsu.ws@hitachi.com">Takashi Norimatsu</a>
+ */
+public class GeneratedEddsaKeyProviderFactory extends AbstractEddsaKeyProviderFactory {
+
+    private static final Logger logger = Logger.getLogger(GeneratedEddsaKeyProviderFactory.class);
+
+    public static final String ID = "eddsa-generated";
+
+    private static final String HELP_TEXT = "Generates EdDSA keys";
+
+    public static final String DEFAULT_EDDSA_ELLIPTIC_CURVE = Algorithm.Ed25519;
+
+    private static final List<ProviderConfigProperty> CONFIG_PROPERTIES = AbstractEddsaKeyProviderFactory.configurationBuilder()
+            .property(EDDSA_ELLIPTIC_CURVE_PROPERTY)
+            .build();
+
+    @Override
+    public KeyProvider create(KeycloakSession session, ComponentModel model) {
+        return new GeneratedEddsaKeyProvider(session.getContext().getRealm(), model);
+    }
+
+    @Override
+    public boolean createFallbackKeys(KeycloakSession session, KeyUse keyUse, String algorithm) {
+        if (keyUse.equals(KeyUse.SIG) && algorithm.equals(Algorithm.EdDSA)) {
+            RealmModel realm = session.getContext().getRealm();
+
+            ComponentModel generated = new ComponentModel();
+            generated.setName("fallback-" + algorithm);
+            generated.setParentId(realm.getId());
+            generated.setProviderId(ID);
+            generated.setProviderType(KeyProvider.class.getName());
+
+            MultivaluedHashMap<String, String> config = new MultivaluedHashMap<>();
+            config.putSingle(Attributes.PRIORITY_KEY, "-100");
+            config.putSingle(EDDSA_ELLIPTIC_CURVE_KEY, DEFAULT_EDDSA_ELLIPTIC_CURVE);
+            generated.setConfig(config);
+
+            realm.addComponentModel(generated);
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String getHelpText() {
+        return HELP_TEXT;
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return CONFIG_PROPERTIES;
+    }
+
+    @Override
+    public String getId() {
+        return ID;
+    }
+
+    @Override
+    public void validateConfiguration(KeycloakSession session, RealmModel realm, ComponentModel model) throws ComponentValidationException {
+        super.validateConfiguration(session, realm, model);
+
+        ConfigurationValidationHelper.check(model).checkList(EDDSA_ELLIPTIC_CURVE_PROPERTY, false);
+
+        String curveName = model.get(EDDSA_ELLIPTIC_CURVE_KEY);
+        if (curveName == null) curveName = DEFAULT_EDDSA_ELLIPTIC_CURVE;
+
+        if (!(model.contains(EDDSA_PRIVATE_KEY_KEY) && model.contains(EDDSA_PUBLIC_KEY_KEY))) {
+            generateKeys(model, curveName);
+            logger.debugv("Generated keys for {0}", realm.getName());
+        } else {
+            String currentEdEc = getCurveFromPublicKey(model.getConfig().getFirst(GeneratedEddsaKeyProviderFactory.EDDSA_PUBLIC_KEY_KEY));
+            if (!curveName.equals(currentEdEc)) {
+                generateKeys(model, curveName);
+                logger.debugv("Twisted Edwards Curve changed, generating new keys for {0}", realm.getName());
+            }
+        }
+    }
+
+    private void generateKeys(ComponentModel model, String curveName) {
+        KeyPair keyPair;
+        try {
+            keyPair = generateEddsaKeyPair(curveName);
+            model.put(EDDSA_PRIVATE_KEY_KEY, Base64.encodeBytes(keyPair.getPrivate().getEncoded()));
+            model.put(EDDSA_PUBLIC_KEY_KEY, Base64.encodeBytes(keyPair.getPublic().getEncoded()));
+            model.put(EDDSA_ELLIPTIC_CURVE_KEY, curveName);
+        } catch (Throwable t) {
+            throw new ComponentValidationException("Failed to generate EdDSA keys", t);
+        }
+    }
+
+    private String getCurveFromPublicKey(String publicEddsaKeyBase64Encoded) {
+        try {
+            KeyFactory kf = KeyFactory.getInstance("EdDSA");
+            X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(Base64.decode(publicEddsaKeyBase64Encoded));
+            EdECPublicKey edEcKey = (EdECPublicKey) kf.generatePublic(publicKeySpec);
+            return edEcKey.getParams().getName();
+        } catch (Throwable t) {
+            throw new ComponentValidationException("Failed to get Twisted Edwards Curve from its public key", t);
+        }
+    }
+}

services/src/test/java/org/keycloak/jose/jwk/ServerJWKTest.java
@@ -1,0 +1,127 @@
+/*
+ * Copyright 2023 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.keycloak.jose.jwk;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.nio.charset.StandardCharsets;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.keycloak.common.util.KeyUtils;
+import org.keycloak.crypto.Algorithm;
+import org.keycloak.crypto.JavaAlgorithm;
+import org.keycloak.rule.CryptoInitRule;
+import org.keycloak.util.JsonSerialization;
+
+/**
+ * This is not tested in keycloak-core. The subclasses should be created in the crypto modules to make sure it is tested with corresponding modules (bouncycastle VS bouncycastle-fips)
+ *
+ * @author <a href="mailto:takashi.norimatsu.ws@hitachi.com">Takashi Norimatsu</a>
+ */
+public abstract class ServerJWKTest {
+
+    @ClassRule
+    public static CryptoInitRule cryptoInitRule = new CryptoInitRule();
+
+
+    @Test
+    public void publicEd25519() throws Exception {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance(Algorithm.Ed25519);
+        KeyPair keyPair = keyGen.generateKeyPair();
+
+        PublicKey publicKey = keyPair.getPublic();
+        JWK jwk = JWKBuilder.create().kid(KeyUtils.createKeyId(keyPair.getPublic())).algorithm(Algorithm.EdDSA).okp(publicKey);
+
+        assertEquals("OKP", jwk.getKeyType());
+        assertEquals("EdDSA", jwk.getAlgorithm());
+        assertEquals("sig", jwk.getPublicKeyUse());
+
+        assertTrue(jwk instanceof OKPPublicJWK);
+
+        OKPPublicJWK okpJwk = (OKPPublicJWK) jwk;
+
+        assertEquals("Ed25519", okpJwk.getCrv());
+        assertNotNull(okpJwk.getX());
+
+        String jwkJson = JsonSerialization.writeValueAsString(jwk);
+
+        JWKParser parser = JWKParser.create().parse(jwkJson);
+        PublicKey publicKeyFromJwk = parser.toPublicKey();
+
+        assertArrayEquals(publicKey.getEncoded(), publicKeyFromJwk.getEncoded());
+
+        byte[] data = "Some test string".getBytes(StandardCharsets.UTF_8);
+        byte[] sign = sign(data, JavaAlgorithm.Ed25519, keyPair.getPrivate());
+        verify(data, sign, JavaAlgorithm.Ed25519, publicKeyFromJwk);
+    }
+
+    @Test
+    public void publicEd448() throws Exception {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance(Algorithm.Ed448);
+        KeyPair keyPair = keyGen.generateKeyPair();
+
+        PublicKey publicKey = keyPair.getPublic();
+        JWK jwk = JWKBuilder.create().kid(KeyUtils.createKeyId(keyPair.getPublic())).algorithm(Algorithm.EdDSA).okp(publicKey);
+
+        assertEquals("OKP", jwk.getKeyType());
+        assertEquals("EdDSA", jwk.getAlgorithm());
+        assertEquals("sig", jwk.getPublicKeyUse());
+
+        assertTrue(jwk instanceof OKPPublicJWK);
+
+        OKPPublicJWK okpJwk = (OKPPublicJWK) jwk;
+
+        assertEquals("Ed448", okpJwk.getCrv());
+        assertNotNull(okpJwk.getX());
+
+        String jwkJson = JsonSerialization.writeValueAsString(jwk);
+
+        JWKParser parser = JWKParser.create().parse(jwkJson);
+        PublicKey publicKeyFromJwk = parser.toPublicKey();
+
+        assertArrayEquals(publicKey.getEncoded(), publicKeyFromJwk.getEncoded());
+
+        byte[] data = "Some test string".getBytes(StandardCharsets.UTF_8);
+        byte[] sign = sign(data, JavaAlgorithm.Ed448, keyPair.getPrivate());
+        verify(data, sign, JavaAlgorithm.Ed448, publicKeyFromJwk);
+    }
+
+    private byte[] sign(byte[] data, String javaAlgorithm, PrivateKey key) throws Exception {
+        Signature signature = Signature.getInstance(javaAlgorithm);
+        signature.initSign(key);
+        signature.update(data);
+        return signature.sign();
+    }
+
+    private boolean verify(byte[] data, byte[] signature, String javaAlgorithm, PublicKey key) throws Exception {
+        Signature verifier = Signature.getInstance(javaAlgorithm);
+        verifier.initVerify(key);
+        verifier.update(data);
+        return verifier.verify(signature);
+    }
+
+}