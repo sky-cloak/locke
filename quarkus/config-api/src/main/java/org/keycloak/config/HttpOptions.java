keycloak/keycloak

Provide histograms for http server metrics
Closes #28178

Signed-off-by: Alexander Schwartz <aschwart@redhat.com>

quarkus/config-api/src/main/java/org/keycloak/config/HttpOptions.java
@@ -132,4 +132,16 @@ public class HttpOptions {
                          "If there are 48 processors it will be 384.")
             .build();
 
+    public static final Option<Boolean> HTTP_METRICS_HISTOGRAMS_ENABLED = new OptionBuilder<>("http-metrics-histograms-enabled", Boolean.class)
+            .category(OptionCategory.HTTP)
+            .description("Enables a histogram with default buckets for the duration of HTTP server requests.")
+            .defaultValue(Boolean.FALSE)
+            .build();
+
+    public static final Option<String> HTTP_METRICS_SLOS = new OptionBuilder<>("http-metrics-slos", String.class)
+            .category(OptionCategory.HTTP)
+            .description("Service level objectives for HTTP server requests. Use this instead of the default histogram, or use it in combination to add additional buckets. " +
+                    "Specify a list of comma-separated vales defined in milliseconds. Example with buckets from 5ms to 10s: 5,10,25,50,250,500,1000,2500,5000,10000")
+            .build();
+
 }

sky-cloak/locke

Co-authored-by: Martin Bartoš <mabartos@redhat.com>
Signed-off-by: Martin Bartoš <mabartos@redhat.com>

quarkus/config-api/src/main/java/org/keycloak/config/HttpOptions.java
@@ -132,4 +132,16 @@ public class HttpOptions {
                          "If there are 48 processors it will be 192.")
             .build();
 
+    public static final Option<Boolean> HTTP_METRICS_HISTOGRAMS_ENABLED = new OptionBuilder<>("http-metrics-histograms-enabled", Boolean.class)
+            .category(OptionCategory.HTTP)
+            .description("Enables a histogram with default buckets for the duration of HTTP server requests.")
+            .defaultValue(Boolean.FALSE)
+            .build();
+
+    public static final Option<String> HTTP_METRICS_SLOS = new OptionBuilder<>("http-metrics-slos", String.class)
+            .category(OptionCategory.HTTP)
+            .description("Service level objectives for HTTP server requests. Use this instead of the default histogram, or use it in combination to add additional buckets. " +
+                    "Specify a list of comma-separated values defined in milliseconds. Example with buckets from 5ms to 10s: 5,10,25,50,250,500,1000,2500,5000,10000")
+            .build();
+
 }