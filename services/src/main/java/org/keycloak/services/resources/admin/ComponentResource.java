keycloak/keycloak

Make sure admin events are not referencing sensitive data from their representation
Closes #21562

services/src/main/java/org/keycloak/services/resources/admin/ComponentResource.java
@@ -144,7 +144,7 @@ public class ComponentResource {
 
                 model = realmModel.addComponentModel(model);
 
-                adminEvent.operation(OperationType.CREATE).resourcePath(kcSession.getContext().getUri(), model.getId()).representation(StripSecretsUtils.strip(kcSession, rep)).success();
+                adminEvent.operation(OperationType.CREATE).resourcePath(kcSession.getContext().getUri(), model.getId()).representation(rep).success();
                 return Response.created(kcSession.getContext().getUri().getAbsolutePathBuilder().path(model.getId()).build()).build();
             } catch (ComponentValidationException e) {
                 return localizedErrorResponse(e);
@@ -185,7 +185,7 @@ public class ComponentResource {
                     throw new NotFoundException("Could not find component");
                 }
                 RepresentationToModel.updateComponent(kcSession, rep, model, false);
-                adminEvent.operation(OperationType.UPDATE).resourcePath(kcSession.getContext().getUri()).representation(StripSecretsUtils.strip(kcSession, rep)).success();
+                adminEvent.operation(OperationType.UPDATE).resourcePath(kcSession.getContext().getUri()).representation(rep).success();
                 realmModel.updateComponent(model);
                 return Response.noContent().build();
             } catch (ComponentValidationException e) {

sky-cloak/locke

Signed-off-by: Joerg Matysiak <joerg.matysiak@bosch.com>

services/src/main/java/org/keycloak/services/resources/admin/ComponentResource.java
@@ -140,7 +140,7 @@ public class ComponentResource {
 
             model = realm.addComponentModel(model);
 
-            adminEvent.operation(OperationType.CREATE).resourcePath(session.getContext().getUri(), model.getId()).representation(StripSecretsUtils.strip(session, rep)).success();
+            adminEvent.operation(OperationType.CREATE).resourcePath(session.getContext().getUri(), model.getId()).representation(rep).success();
             return Response.created(session.getContext().getUri().getAbsolutePathBuilder().path(model.getId()).build()).build();
         } catch (ComponentValidationException e) {
             return localizedErrorResponse(e);
@@ -178,7 +178,7 @@ public class ComponentResource {
                 throw new NotFoundException("Could not find component");
             }
             RepresentationToModel.updateComponent(session, rep, model, false);
-            adminEvent.operation(OperationType.UPDATE).resourcePath(session.getContext().getUri()).representation(StripSecretsUtils.strip(session, rep)).success();
+            adminEvent.operation(OperationType.UPDATE).resourcePath(session.getContext().getUri()).representation(rep).success();
             realm.updateComponent(model);
             return Response.noContent().build();
         } catch (ComponentValidationException e) {