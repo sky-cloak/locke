lexcao/keycloak

Fallback to no override flow when missing in client override
Closes #30765

Signed-off-by: Lex Cao <lexcao@foxmail.com>

server-spi-private/src/main/java/org/keycloak/models/utils/AuthenticationFlowResolver.java
@@ -16,10 +16,10 @@
  */
 package org.keycloak.models.utils;
 
+import org.jboss.logging.Logger;
 import org.keycloak.models.AuthenticationFlowBindings;
 import org.keycloak.models.AuthenticationFlowModel;
 import org.keycloak.models.ClientModel;
-import org.keycloak.models.ModelException;
 import org.keycloak.sessions.AuthenticationSessionModel;
 
 /**
@@ -28,16 +28,19 @@ import org.keycloak.sessions.AuthenticationSessionModel;
  */
 public class AuthenticationFlowResolver {
 
+    private static final Logger logger = Logger.getLogger(AuthenticationFlowResolver.class);
+
     public static AuthenticationFlowModel resolveBrowserFlow(AuthenticationSessionModel authSession) {
         AuthenticationFlowModel flow = null;
         ClientModel client = authSession.getClient();
         String clientFlow = client.getAuthenticationFlowBindingOverride(AuthenticationFlowBindings.BROWSER_BINDING);
         if (clientFlow != null) {
             flow = authSession.getRealm().getAuthenticationFlowById(clientFlow);
-            if (flow == null) {
-                throw new ModelException("Client " + client.getClientId() + " has browser flow override, but this flow does not exist");
+            if (flow != null) {
+                return flow;
             }
-            return flow;
+            logger.warnf("Client %s has browser flow override, but this flow '%s' does not exist, " +
+                    "fallback to browser flow", client.getClientId(), flow);
         }
         return authSession.getRealm().getBrowserFlow();
     }
@@ -47,10 +50,11 @@ public class AuthenticationFlowResolver {
         String clientFlow = client.getAuthenticationFlowBindingOverride(AuthenticationFlowBindings.DIRECT_GRANT_BINDING);
         if (clientFlow != null) {
             flow = authSession.getRealm().getAuthenticationFlowById(clientFlow);
-            if (flow == null) {
-                throw new ModelException("Client " + client.getClientId() + " has direct grant flow override, but this flow does not exist");
+            if (flow != null) {
+                return flow;
             }
-            return flow;
+            logger.warnf("Client %s has direct grant flow override, but this flow '%s' does not exist, " +
+                    "fallback to direct grant flow", client.getClientId(), flow);
         }
         return authSession.getRealm().getDirectGrantFlow();
     }

sky-cloak/locke

server-spi-private/src/main/java/org/keycloak/models/utils/AuthenticationFlowResolver.java
@@ -16,10 +16,10 @@
  */
 package org.keycloak.models.utils;
 
+import org.jboss.logging.Logger;
 import org.keycloak.models.AuthenticationFlowBindings;
 import org.keycloak.models.AuthenticationFlowModel;
 import org.keycloak.models.ClientModel;
-import org.keycloak.models.ModelException;
 import org.keycloak.sessions.AuthenticationSessionModel;
 
 /**
@@ -28,16 +28,19 @@ import org.keycloak.sessions.AuthenticationSessionModel;
  */
 public class AuthenticationFlowResolver {
 
+    private static final Logger logger = Logger.getLogger(AuthenticationFlowResolver.class);
+
     public static AuthenticationFlowModel resolveBrowserFlow(AuthenticationSessionModel authSession) {
         AuthenticationFlowModel flow = null;
         ClientModel client = authSession.getClient();
         String clientFlow = client.getAuthenticationFlowBindingOverride(AuthenticationFlowBindings.BROWSER_BINDING);
         if (clientFlow != null) {
             flow = authSession.getRealm().getAuthenticationFlowById(clientFlow);
-            if (flow == null) {
-                throw new ModelException("Client " + client.getClientId() + " has browser flow override, but this flow does not exist");
+            if (flow != null) {
+                return flow;
             }
-            return flow;
+            logger.warnf("Client %s has browser flow override, but this flow '%s' does not exist, " +
+                    "fallback to browser flow", client.getClientId(), clientFlow);
         }
         return authSession.getRealm().getBrowserFlow();
     }
@@ -47,10 +50,11 @@ public class AuthenticationFlowResolver {
         String clientFlow = client.getAuthenticationFlowBindingOverride(AuthenticationFlowBindings.DIRECT_GRANT_BINDING);
         if (clientFlow != null) {
             flow = authSession.getRealm().getAuthenticationFlowById(clientFlow);
-            if (flow == null) {
-                throw new ModelException("Client " + client.getClientId() + " has direct grant flow override, but this flow does not exist");
+            if (flow != null) {
+                return flow;
             }
-            return flow;
+            logger.warnf("Client %s has direct grant flow override, but this flow '%s' does not exist, " +
+                    "fallback to direct grant flow", client.getClientId(), clientFlow);
         }
         return authSession.getRealm().getDirectGrantFlow();
     }