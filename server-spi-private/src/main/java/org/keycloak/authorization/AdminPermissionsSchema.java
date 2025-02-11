keycloak/keycloak

Add Client resource type and its scopes to authorization schema and evaluation implementation for ClientsPermissionsV2
Closes #35564

Signed-off-by: Martin Kanis <mkanis@redhat.com>

server-spi-private/src/main/java/org/keycloak/authorization/AdminPermissionsSchema.java
@@ -49,8 +49,9 @@ import org.keycloak.representations.idm.authorization.ScopePermissionRepresentat
 import org.keycloak.representations.idm.authorization.ScopeRepresentation;
 
 public class AdminPermissionsSchema extends AuthorizationSchema {
-
+    
     public static final String USERS_RESOURCE_TYPE = "Users";
+    public static final String CLIENTS_RESOURCE_TYPE = "Clients";
 
     //scopes
     public static final String MANAGE = "manage";
@@ -58,13 +59,18 @@ public class AdminPermissionsSchema extends AuthorizationSchema {
     public static final String IMPERSONATE = "impersonate";
     public static final String MAP_ROLES = "map-roles";
     public static final String MANAGE_GROUP_MEMBERSHIP = "manage-group-membership";
+    public static final String CONFIGURE = "configure";
+    public static final String MAP_ROLES_CLIENT_SCOPE = "map-roles-client-scope";
+    public static final String MAP_ROLES_COMPOSITE = "map-roles-composite";
+    public static final String TOKEN_EXCHANGE = "token-exchange";
 
     public static final ResourceType USERS = new ResourceType(USERS_RESOURCE_TYPE, Set.of(MANAGE, VIEW, IMPERSONATE, MAP_ROLES, MANAGE_GROUP_MEMBERSHIP));
+    public static final ResourceType CLIENTS = new ResourceType(CLIENTS_RESOURCE_TYPE, Set.of(CONFIGURE, MANAGE, MAP_ROLES, MAP_ROLES_CLIENT_SCOPE, MAP_ROLES_COMPOSITE, TOKEN_EXCHANGE, VIEW));
 
     public static final AdminPermissionsSchema SCHEMA = new AdminPermissionsSchema();
 
     private AdminPermissionsSchema() {
-        super(Map.of(USERS_RESOURCE_TYPE, USERS));
+        super(Map.of(USERS_RESOURCE_TYPE, USERS, CLIENTS_RESOURCE_TYPE, CLIENTS));
     }
 
     public Resource getOrCreateResource(KeycloakSession session, ResourceServer resourceServer, String type, String id) {
@@ -78,12 +84,15 @@ public class AdminPermissionsSchema extends AuthorizationSchema {
 
         if (USERS.getType().equals(type)) {
             name = resolveUser(session, id);
-            if (name == null) {
-                Resource resource = resourceStore.findById(resourceServer, id);
+        } else if (CLIENTS.getType().equals(type)) {
+            name = resolveClient(session, id);
+        }
 
-                if (resource != null) {
-                    name = resource.getName();
-                }
+        if (name == null) {
+            Resource resource = resourceStore.findById(resourceServer, id);
+
+            if (resource != null) {
+                name = resource.getName();
             }
         }
 
@@ -144,6 +153,17 @@ public class AdminPermissionsSchema extends AuthorizationSchema {
         }
 
         return user == null ? null : user.getId();
+    }
+
+    private String resolveClient(KeycloakSession session, String id) {
+        RealmModel realm = session.getContext().getRealm();
+        ClientModel client = session.clients().getClientById(realm, id);
+
+        if (client == null) {
+            client = session.clients().getClientByClientId(realm, id);
+        }
+
+        return client == null ? null : client.getId();
     }
 
     private StoreFactory getStoreFactory(KeycloakSession session) {

sky-cloak/locke

server-spi-private/src/main/java/org/keycloak/authorization/AdminPermissionsSchema.java
@@ -50,8 +50,9 @@ import org.keycloak.representations.idm.authorization.ScopePermissionRepresentat
 import org.keycloak.representations.idm.authorization.ScopeRepresentation;
 
 public class AdminPermissionsSchema extends AuthorizationSchema {
-
+    
     public static final String USERS_RESOURCE_TYPE = "Users";
+    public static final String CLIENTS_RESOURCE_TYPE = "Clients";
 
     //scopes
     public static final String MANAGE = "manage";
@@ -59,13 +60,17 @@ public class AdminPermissionsSchema extends AuthorizationSchema {
     public static final String IMPERSONATE = "impersonate";
     public static final String MAP_ROLES = "map-roles";
     public static final String MANAGE_GROUP_MEMBERSHIP = "manage-group-membership";
+    public static final String CONFIGURE = "configure";
+    public static final String MAP_ROLES_CLIENT_SCOPE = "map-roles-client-scope";
+    public static final String MAP_ROLES_COMPOSITE = "map-roles-composite";
 
     public static final ResourceType USERS = new ResourceType(USERS_RESOURCE_TYPE, Set.of(MANAGE, VIEW, IMPERSONATE, MAP_ROLES, MANAGE_GROUP_MEMBERSHIP));
+    public static final ResourceType CLIENTS = new ResourceType(CLIENTS_RESOURCE_TYPE, Set.of(CONFIGURE, MANAGE, MAP_ROLES, MAP_ROLES_CLIENT_SCOPE, MAP_ROLES_COMPOSITE, VIEW));
 
     public static final AdminPermissionsSchema SCHEMA = new AdminPermissionsSchema();
 
     private AdminPermissionsSchema() {
-        super(Map.of(USERS_RESOURCE_TYPE, USERS));
+        super(Map.of(USERS_RESOURCE_TYPE, USERS, CLIENTS_RESOURCE_TYPE, CLIENTS));
     }
 
     public Resource getOrCreateResource(KeycloakSession session, ResourceServer resourceServer, String policyType, String resourceType, String id) {
@@ -85,6 +90,8 @@ public class AdminPermissionsSchema extends AuthorizationSchema {
 
         if (USERS.getType().equals(resourceType)) {
             name = resolveUser(session, id);
+        } else if (CLIENTS.getType().equals(resourceType)) {
+            name = resolveClient(session, id);
         }
 
         if (name == null) {
@@ -163,6 +170,17 @@ public class AdminPermissionsSchema extends AuthorizationSchema {
         }
 
         return user == null ? null : user.getId();
+    }
+
+    private String resolveClient(KeycloakSession session, String id) {
+        RealmModel realm = session.getContext().getRealm();
+        ClientModel client = session.clients().getClientById(realm, id);
+
+        if (client == null) {
+            client = session.clients().getClientByClientId(realm, id);
+        }
+
+        return client == null ? null : client.getId();
     }
 
     private StoreFactory getStoreFactory(KeycloakSession session) {