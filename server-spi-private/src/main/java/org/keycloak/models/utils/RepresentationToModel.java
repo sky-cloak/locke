keycloak/keycloak

Allow redirection to idp when user email matches any of the org domains
Signed-off-by: Stefan Guilhen <sguilhen@redhat.com>

Closes #33804

server-spi-private/src/main/java/org/keycloak/models/utils/RepresentationToModel.java
@@ -131,6 +131,7 @@ import org.keycloak.util.JsonSerialization;
 import org.keycloak.utils.StringUtil;
 
 import static java.util.Optional.ofNullable;
+import static org.keycloak.models.OrganizationDomainModel.ANY_DOMAIN;
 import static org.keycloak.protocol.saml.util.ArtifactBindingUtils.computeArtifactBindingIdentifierString;
 
 public class RepresentationToModel {
@@ -1703,7 +1704,7 @@ public class RepresentationToModel {
 
             if (StringUtil.isBlank(domain)) {
                 representation.getConfig().remove(OrganizationModel.ORGANIZATION_DOMAIN_ATTRIBUTE);
-            } else if (org.getDomains().map(OrganizationDomainModel::getName).noneMatch(domain::equals)) {
+            } else if (!ANY_DOMAIN.equalsIgnoreCase(domain) && org.getDomains().map(OrganizationDomainModel::getName).noneMatch(domain::equals)) {
                 throw new IllegalArgumentException("Domain does not match any domain from the organization");
             }
 

services/src/main/java/org/keycloak/organization/validator/OrganizationMemberValidator.java
@@ -18,6 +18,7 @@
 package org.keycloak.organization.validator;
 
 import static java.util.Optional.ofNullable;
+import static org.keycloak.models.OrganizationDomainModel.ANY_DOMAIN;
 import static org.keycloak.organization.utils.Organizations.resolveHomeBroker;
 import static org.keycloak.validate.BuiltinValidators.emailValidator;
 
@@ -110,7 +111,7 @@ public class OrganizationMemberValidator extends AbstractSimpleValidator impleme
         if (UserProfileContext.IDP_REVIEW.equals(attributeContext.getContext())) {
             expectedDomains = resolveExpectedDomainsWhenReviewingFederatedUserProfile(organization, attributeContext);
         } else if (organization.isManaged(user)) {
-            expectedDomains = resolveExpectedDomainsForManagedUser(context, user);
+            expectedDomains = resolveExpectedDomainsForManagedUser(organization, context, user);
         } else {
             // no validation happens for unmanaged users as they are realm users linked to an organization
             return;
@@ -124,7 +125,7 @@ public class OrganizationMemberValidator extends AbstractSimpleValidator impleme
         context.addError(new ValidationError(ID, inputHint, "Email domain does not match any domain from the organization"));
     }
 
-    private static Set<String> resolveExpectedDomainsForManagedUser(ValidationContext context, UserModel user) {
+    private static Set<String> resolveExpectedDomainsForManagedUser(OrganizationModel organization, ValidationContext context, UserModel user) {
         List<IdentityProviderModel> brokers = resolveHomeBroker(context.getSession(), user);
 
         if (brokers.isEmpty()) {
@@ -135,7 +136,9 @@ public class OrganizationMemberValidator extends AbstractSimpleValidator impleme
 
         for (IdentityProviderModel broker : brokers) {
             String domain = broker.getConfig().get(OrganizationModel.ORGANIZATION_DOMAIN_ATTRIBUTE);
-
+            if (ANY_DOMAIN.equalsIgnoreCase(domain)) {
+                organization.getDomains().map(OrganizationDomainModel::getName).forEach(domains::add);
+            }
             if (domain != null) {
                 domains.add(domain);
             }
@@ -166,6 +169,9 @@ public class OrganizationMemberValidator extends AbstractSimpleValidator impleme
 
         // expect the email domain to match the domain set to the broker or none if not set
         String brokerDomain = broker.getConfig().get(OrganizationModel.ORGANIZATION_DOMAIN_ATTRIBUTE);
+        if (ANY_DOMAIN.equalsIgnoreCase(brokerDomain)) {
+            return organization.getDomains().map(OrganizationDomainModel::getName).collect(Collectors.toSet());
+        }
         return  ofNullable(brokerDomain).map(Set::of).orElse(Set.of());
     }
 }

sky-cloak/locke

Co-authored-by: Martin Panzer <martin.panzer@active-logistics.com>

server-spi-private/src/main/java/org/keycloak/models/utils/RepresentationToModel.java
@@ -131,6 +131,7 @@ import org.keycloak.util.JsonSerialization;
 import org.keycloak.utils.StringUtil;
 
 import static java.util.Optional.ofNullable;
+import static org.keycloak.models.OrganizationDomainModel.ANY_DOMAIN;
 import static org.keycloak.protocol.saml.util.ArtifactBindingUtils.computeArtifactBindingIdentifierString;
 
 public class RepresentationToModel {
@@ -1703,7 +1704,7 @@ public class RepresentationToModel {
 
             if (StringUtil.isBlank(domain)) {
                 representation.getConfig().remove(OrganizationModel.ORGANIZATION_DOMAIN_ATTRIBUTE);
-            } else if (org.getDomains().map(OrganizationDomainModel::getName).noneMatch(domain::equals)) {
+            } else if (!ANY_DOMAIN.equals(domain) && org.getDomains().map(OrganizationDomainModel::getName).noneMatch(domain::equals)) {
                 throw new IllegalArgumentException("Domain does not match any domain from the organization");
             }
 

services/src/main/java/org/keycloak/organization/validator/OrganizationMemberValidator.java
@@ -18,6 +18,7 @@
 package org.keycloak.organization.validator;
 
 import static java.util.Optional.ofNullable;
+import static org.keycloak.models.OrganizationDomainModel.ANY_DOMAIN;
 import static org.keycloak.organization.utils.Organizations.resolveHomeBroker;
 import static org.keycloak.validate.BuiltinValidators.emailValidator;
 
@@ -110,7 +111,7 @@ public class OrganizationMemberValidator extends AbstractSimpleValidator impleme
         if (UserProfileContext.IDP_REVIEW.equals(attributeContext.getContext())) {
             expectedDomains = resolveExpectedDomainsWhenReviewingFederatedUserProfile(organization, attributeContext);
         } else if (organization.isManaged(user)) {
-            expectedDomains = resolveExpectedDomainsForManagedUser(context, user);
+            expectedDomains = resolveExpectedDomainsForManagedUser(organization, context, user);
         } else {
             // no validation happens for unmanaged users as they are realm users linked to an organization
             return;
@@ -124,7 +125,7 @@ public class OrganizationMemberValidator extends AbstractSimpleValidator impleme
         context.addError(new ValidationError(ID, inputHint, "Email domain does not match any domain from the organization"));
     }
 
-    private static Set<String> resolveExpectedDomainsForManagedUser(ValidationContext context, UserModel user) {
+    private static Set<String> resolveExpectedDomainsForManagedUser(OrganizationModel organization, ValidationContext context, UserModel user) {
         List<IdentityProviderModel> brokers = resolveHomeBroker(context.getSession(), user);
 
         if (brokers.isEmpty()) {
@@ -135,8 +136,10 @@ public class OrganizationMemberValidator extends AbstractSimpleValidator impleme
 
         for (IdentityProviderModel broker : brokers) {
             String domain = broker.getConfig().get(OrganizationModel.ORGANIZATION_DOMAIN_ATTRIBUTE);
-
-            if (domain != null) {
+            if (ANY_DOMAIN.equals(domain)) {
+                organization.getDomains().map(OrganizationDomainModel::getName).forEach(domains::add);
+            }
+            else if (domain != null) {
                 domains.add(domain);
             }
         }
@@ -166,6 +169,9 @@ public class OrganizationMemberValidator extends AbstractSimpleValidator impleme
 
         // expect the email domain to match the domain set to the broker or none if not set
         String brokerDomain = broker.getConfig().get(OrganizationModel.ORGANIZATION_DOMAIN_ATTRIBUTE);
+        if (ANY_DOMAIN.equals(brokerDomain)) {
+            return organization.getDomains().map(OrganizationDomainModel::getName).collect(Collectors.toSet());
+        }
         return  ofNullable(brokerDomain).map(Set::of).orElse(Set.of());
     }
 }