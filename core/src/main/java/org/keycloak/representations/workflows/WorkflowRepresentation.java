keycloak/keycloak

Rename RLM to Workflows
Closes #42512

Signed-off-by: vramik <vramik@redhat.com>

core/src/main/java/org/keycloak/representations/workflows/WorkflowRepresentation.java
@@ -1,0 +1,179 @@
+package org.keycloak.representations.workflows;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+
+import org.keycloak.common.util.MultivaluedHashMap;
+
+public class WorkflowRepresentation {
+
+    public static Builder create() {
+        return new Builder();
+    }
+
+    private String id;
+    private String providerId;
+    private MultivaluedHashMap<String, String> config;
+    private List<WorkflowActionRepresentation> actions;
+    private List<WorkflowConditionRepresentation> conditions;
+
+    public WorkflowRepresentation() {
+        // reflection
+    }
+
+    public WorkflowRepresentation(String providerId) {
+        this(providerId, null);
+    }
+
+    public WorkflowRepresentation(String providerId, Map<String, List<String>> config) {
+        this(null, providerId, config);
+    }
+
+    public WorkflowRepresentation(String id, String providerId, Map<String, List<String>> config) {
+        this.id = id;
+        this.providerId = providerId;
+        this.config = new MultivaluedHashMap<>(config);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProviderId() {
+        return this.providerId;
+    }
+
+    public void setProviderId(String providerId) {
+        this.providerId = providerId;
+    }
+
+    public String getName() {
+        return Optional.ofNullable(config).orElse(new MultivaluedHashMap<>()).getFirst("name");
+    }
+
+    public void setName(String name) {
+        if (this.config == null) {
+            this.config = new MultivaluedHashMap<>();
+        }
+        this.config.putSingle("name", name);
+    }
+
+    public void setConditions(List<WorkflowConditionRepresentation> conditions) {
+        this.conditions = conditions;
+    }
+
+    public List<WorkflowConditionRepresentation> getConditions() {
+        return conditions;
+    }
+
+    public void setActions(List<WorkflowActionRepresentation> actions) {
+        this.actions = actions;
+    }
+
+    public List<WorkflowActionRepresentation> getActions() {
+        return actions;
+    }
+
+    public MultivaluedHashMap<String, String> getConfig() {
+        return config;
+    }
+
+    public void addAction(WorkflowActionRepresentation action) {
+        if (actions == null) {
+            actions = new ArrayList<>();
+        }
+        actions.add(action);
+    }
+
+    public static class Builder {
+        private String providerId;
+        private final Map<String, List<String>> config = new HashMap<>();
+        private List<WorkflowConditionRepresentation> conditions = new ArrayList<>();
+        private final Map<String, List<WorkflowActionRepresentation>> actions = new HashMap<>();
+        private List<Builder> builders = new ArrayList<>();
+
+        private Builder() {
+        }
+
+        private Builder(String providerId, List<Builder> builders) {
+            this.providerId = providerId;
+            this.builders = builders;
+        }
+
+        public Builder of(String providerId) {
+            Builder builder = new Builder(providerId, builders);
+            builders.add(builder);
+            return builder;
+        }
+
+        public Builder onEvent(String operation) {
+            List<String> events = config.computeIfAbsent("events", k -> new ArrayList<>());
+
+            events.add(operation);
+
+            return this;
+        }
+
+        public Builder onConditions(WorkflowConditionRepresentation... condition) {
+            if (conditions == null) {
+                conditions = new ArrayList<>();
+            }
+            conditions.addAll(Arrays.asList(condition));
+            return this;
+        }
+
+        public Builder withActions(WorkflowActionRepresentation... actions) {
+            this.actions.computeIfAbsent(providerId, (k) -> new ArrayList<>()).addAll(Arrays.asList(actions));
+            return this;
+        }
+
+        public Builder withConfig(String key, String value) {
+            config.put(key, Collections.singletonList(value));
+            return this;
+        }
+
+        public Builder withConfig(String key, List<String> value) {
+            config.put(key, value);
+            return this;
+        }
+
+        public Builder name(String name) {
+            return withConfig("name", name);
+        }
+
+        public Builder immediate() {
+            return withConfig("scheduled", "false");
+        }
+
+        public Builder recurring() {
+            return withConfig("recurring", "true");
+        }
+
+        public List<WorkflowRepresentation> build() {
+            List<WorkflowRepresentation> workflows = new ArrayList<>();
+
+            for (Builder builder : builders) {
+                for (Entry<String, List<WorkflowActionRepresentation>> entry : builder.actions.entrySet()) {
+                    WorkflowRepresentation workflow = new WorkflowRepresentation(entry.getKey(), builder.config);
+
+                    workflow.setActions(entry.getValue());
+                    workflow.setConditions(builder.conditions);
+
+                    workflows.add(workflow);
+                }
+            }
+
+            return workflows;
+        }
+    }
+}

tests/base/src/test/java/org/keycloak/tests/admin/model/workflow/UserCreationTimeWorkflowTest.java
@@ -1,0 +1,154 @@
+package org.keycloak.tests.admin.model.workflow;
+
+import jakarta.mail.internet.MimeMessage;
+import org.junit.jupiter.api.Test;
+import org.keycloak.common.util.Time;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.models.workflow.DisableUserActionProviderFactory;
+import org.keycloak.models.workflow.NotifyUserActionProviderFactory;
+import org.keycloak.models.workflow.WorkflowsManager;
+import org.keycloak.models.workflow.UserCreationTimeWorkflowProviderFactory;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.keycloak.representations.workflows.WorkflowActionRepresentation;
+import org.keycloak.representations.workflows.WorkflowRepresentation;
+import org.keycloak.testframework.annotations.InjectRealm;
+import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
+import org.keycloak.testframework.mail.MailServer;
+import org.keycloak.testframework.mail.annotations.InjectMailServer;
+import org.keycloak.testframework.oauth.OAuthClient;
+import org.keycloak.testframework.oauth.annotations.InjectOAuthClient;
+import org.keycloak.testframework.realm.ManagedRealm;
+import org.keycloak.testframework.remote.runonserver.InjectRunOnServer;
+import org.keycloak.testframework.remote.runonserver.RunOnServerClient;
+import org.keycloak.testframework.ui.annotations.InjectPage;
+import org.keycloak.testframework.ui.annotations.InjectWebDriver;
+import org.keycloak.testframework.ui.page.LoginPage;
+import org.openqa.selenium.WebDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.keycloak.tests.admin.model.workflow.WorkflowManagementTest.findEmailByRecipient;
+
+@KeycloakIntegrationTest(config = WorkflowsServerConfig.class)
+public class UserCreationTimeWorkflowTest {
+
+    private static final String REALM_NAME = "default";
+
+    @InjectRunOnServer(permittedPackages = "org.keycloak.tests")
+    RunOnServerClient runOnServer;
+
+    @InjectRealm
+    ManagedRealm managedRealm;
+
+    @InjectWebDriver
+    WebDriver driver;
+
+    @InjectPage
+    LoginPage loginPage;
+
+    @InjectOAuthClient
+    OAuthClient oauth;
+
+    @InjectMailServer
+    private MailServer mailServer;
+
+    @Test
+    public void testDisableUserBasedOnCreationDate() {
+        managedRealm.admin().workflows().create(WorkflowRepresentation.create()
+                .of(UserCreationTimeWorkflowProviderFactory.ID)
+                .withActions(
+                        WorkflowActionRepresentation.create().of(NotifyUserActionProviderFactory.ID)
+                                .after(Duration.ofDays(5))
+                                .build(),
+                        WorkflowActionRepresentation.create().of(DisableUserActionProviderFactory.ID)
+                                .after(Duration.ofDays(10))
+                                .build()
+                ).build()).close();
+
+        // create a new user - this will trigger the association with the workflow
+        managedRealm.admin().users().create(
+                this.getUserRepresentation("alice", "Alice", "Wonderland", "alice@wornderland.org")).close();
+
+        // test running the scheduled actions
+        runOnServer.run((session -> {
+            RealmModel realm = configureSessionContext(session);
+            WorkflowsManager manager = new WorkflowsManager(session);
+
+            UserModel user = session.users().getUserByUsername(realm, "alice");
+            assertTrue(user.isEnabled());
+            assertNull(user.getAttributes().get("message"));
+
+            // running the scheduled tasks now shouldn't pick up any action as none are due to run yet
+            manager.runScheduledActions();
+            user = session.users().getUserByUsername(realm, "alice");
+            assertTrue(user.isEnabled());
+            assertNull(user.getAttributes().get("message"));
+
+            try {
+                // set offset to 7 days - notify action should run now
+                Time.setOffset(Math.toIntExact(Duration.ofDays(6).toSeconds()));
+                manager.runScheduledActions();
+                user = session.users().getUserByUsername(realm, "alice");
+                assertTrue(user.isEnabled());
+            } finally {
+                Time.setOffset(0);
+            }
+        }));
+
+        // Verify that the notify action was executed by checking email was sent
+        MimeMessage testUserMessage = findEmailByRecipient(mailServer, "alice@wornderland.org");
+        assertNotNull(testUserMessage, "The first action (notify) should have sent an email.");
+
+        mailServer.runCleanup();
+
+        // logging-in with alice should not reset the workflow - we should still run the disable action next
+        oauth.openLoginForm();
+        loginPage.fillLogin("alice", "alice");
+        loginPage.submit();
+        assertTrue(driver.getPageSource().contains("Happy days"));
+
+        // test running the scheduled actions
+        runOnServer.run((session -> {
+            RealmModel realm = configureSessionContext(session);
+            WorkflowsManager manager = new WorkflowsManager(session);
+
+            try {
+                // set offset to 11 days - disable action should run now
+                Time.setOffset(Math.toIntExact(Duration.ofDays(12).toSeconds()));
+                manager.runScheduledActions();
+                UserModel user = session.users().getUserByUsername(realm, "alice");
+                assertFalse(user.isEnabled());
+            } finally {
+                Time.setOffset(0);
+            }
+        }));
+    }
+
+    private static RealmModel configureSessionContext(KeycloakSession session) {
+        RealmModel realm = session.realms().getRealmByName(REALM_NAME);
+        session.getContext().setRealm(realm);
+        return realm;
+    }
+
+    private UserRepresentation getUserRepresentation(String username, String firstName, String lastName, String email) {
+        UserRepresentation representation = new UserRepresentation();
+        representation.setUsername(username);
+        representation.setFirstName(firstName);
+        representation.setLastName(lastName);
+        representation.setEmail(email);
+        representation.setEnabled(true);
+        CredentialRepresentation credential = new CredentialRepresentation();
+        credential.setType(CredentialRepresentation.PASSWORD);
+        credential.setValue(username);
+        representation.setCredentials(List.of(credential));
+        return representation;
+    }
+}

tests/base/src/test/java/org/keycloak/tests/admin/model/workflow/WorkflowsScheduledTaskServerConfig.java
@@ -1,0 +1,13 @@
+package org.keycloak.tests.admin.model.workflow;
+
+import org.keycloak.models.workflow.WorkflowsEventListenerFactory;
+import org.keycloak.testframework.server.KeycloakServerConfigBuilder;
+
+public class WorkflowsScheduledTaskServerConfig extends WorkflowsServerConfig {
+
+    @Override
+    public KeycloakServerConfigBuilder configure(KeycloakServerConfigBuilder config) {
+        return super.configure(config)
+                .option("spi-events-listener--" + WorkflowsEventListenerFactory.ID + "--action-runner-task-interval", "1000");
+    }
+}

sky-cloak/locke

core/src/main/java/org/keycloak/representations/workflows/WorkflowRepresentation.java
@@ -1,0 +1,179 @@
+package org.keycloak.representations.workflows;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+
+import org.keycloak.common.util.MultivaluedHashMap;
+
+public class WorkflowRepresentation {
+
+    public static Builder create() {
+        return new Builder();
+    }
+
+    private String id;
+    private String providerId;
+    private MultivaluedHashMap<String, String> config;
+    private List<WorkflowStepRepresentation> steps;
+    private List<WorkflowConditionRepresentation> conditions;
+
+    public WorkflowRepresentation() {
+        // reflection
+    }
+
+    public WorkflowRepresentation(String providerId) {
+        this(providerId, null);
+    }
+
+    public WorkflowRepresentation(String providerId, Map<String, List<String>> config) {
+        this(null, providerId, config);
+    }
+
+    public WorkflowRepresentation(String id, String providerId, Map<String, List<String>> config) {
+        this.id = id;
+        this.providerId = providerId;
+        this.config = new MultivaluedHashMap<>(config);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProviderId() {
+        return this.providerId;
+    }
+
+    public void setProviderId(String providerId) {
+        this.providerId = providerId;
+    }
+
+    public String getName() {
+        return Optional.ofNullable(config).orElse(new MultivaluedHashMap<>()).getFirst("name");
+    }
+
+    public void setName(String name) {
+        if (this.config == null) {
+            this.config = new MultivaluedHashMap<>();
+        }
+        this.config.putSingle("name", name);
+    }
+
+    public void setConditions(List<WorkflowConditionRepresentation> conditions) {
+        this.conditions = conditions;
+    }
+
+    public List<WorkflowConditionRepresentation> getConditions() {
+        return conditions;
+    }
+
+    public void setSteps(List<WorkflowStepRepresentation> steps) {
+        this.steps = steps;
+    }
+
+    public List<WorkflowStepRepresentation> getSteps() {
+        return steps;
+    }
+
+    public MultivaluedHashMap<String, String> getConfig() {
+        return config;
+    }
+
+    public void addStep(WorkflowStepRepresentation step) {
+        if (steps == null) {
+            steps = new ArrayList<>();
+        }
+        steps.add(step);
+    }
+
+    public static class Builder {
+        private String providerId;
+        private final Map<String, List<String>> config = new HashMap<>();
+        private List<WorkflowConditionRepresentation> conditions = new ArrayList<>();
+        private final Map<String, List<WorkflowStepRepresentation>> steps = new HashMap<>();
+        private List<Builder> builders = new ArrayList<>();
+
+        private Builder() {
+        }
+
+        private Builder(String providerId, List<Builder> builders) {
+            this.providerId = providerId;
+            this.builders = builders;
+        }
+
+        public Builder of(String providerId) {
+            Builder builder = new Builder(providerId, builders);
+            builders.add(builder);
+            return builder;
+        }
+
+        public Builder onEvent(String operation) {
+            List<String> events = config.computeIfAbsent("events", k -> new ArrayList<>());
+
+            events.add(operation);
+
+            return this;
+        }
+
+        public Builder onConditions(WorkflowConditionRepresentation... condition) {
+            if (conditions == null) {
+                conditions = new ArrayList<>();
+            }
+            conditions.addAll(Arrays.asList(condition));
+            return this;
+        }
+
+        public Builder withSteps(WorkflowStepRepresentation... steps) {
+            this.steps.computeIfAbsent(providerId, (k) -> new ArrayList<>()).addAll(Arrays.asList(steps));
+            return this;
+        }
+
+        public Builder withConfig(String key, String value) {
+            config.put(key, Collections.singletonList(value));
+            return this;
+        }
+
+        public Builder withConfig(String key, List<String> value) {
+            config.put(key, value);
+            return this;
+        }
+
+        public Builder name(String name) {
+            return withConfig("name", name);
+        }
+
+        public Builder immediate() {
+            return withConfig("scheduled", "false");
+        }
+
+        public Builder recurring() {
+            return withConfig("recurring", "true");
+        }
+
+        public List<WorkflowRepresentation> build() {
+            List<WorkflowRepresentation> workflows = new ArrayList<>();
+
+            for (Builder builder : builders) {
+                for (Entry<String, List<WorkflowStepRepresentation>> entry : builder.steps.entrySet()) {
+                    WorkflowRepresentation workflow = new WorkflowRepresentation(entry.getKey(), builder.config);
+
+                    workflow.setSteps(entry.getValue());
+                    workflow.setConditions(builder.conditions);
+
+                    workflows.add(workflow);
+                }
+            }
+
+            return workflows;
+        }
+    }
+}

tests/base/src/test/java/org/keycloak/tests/admin/model/workflow/UserCreationTimeWorkflowTest.java
@@ -1,0 +1,154 @@
+package org.keycloak.tests.admin.model.workflow;
+
+import jakarta.mail.internet.MimeMessage;
+import org.junit.jupiter.api.Test;
+import org.keycloak.common.util.Time;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.models.workflow.DisableUserStepProviderFactory;
+import org.keycloak.models.workflow.NotifyUserStepProviderFactory;
+import org.keycloak.models.workflow.WorkflowsManager;
+import org.keycloak.models.workflow.UserCreationTimeWorkflowProviderFactory;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.keycloak.representations.workflows.WorkflowStepRepresentation;
+import org.keycloak.representations.workflows.WorkflowRepresentation;
+import org.keycloak.testframework.annotations.InjectRealm;
+import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
+import org.keycloak.testframework.mail.MailServer;
+import org.keycloak.testframework.mail.annotations.InjectMailServer;
+import org.keycloak.testframework.oauth.OAuthClient;
+import org.keycloak.testframework.oauth.annotations.InjectOAuthClient;
+import org.keycloak.testframework.realm.ManagedRealm;
+import org.keycloak.testframework.remote.runonserver.InjectRunOnServer;
+import org.keycloak.testframework.remote.runonserver.RunOnServerClient;
+import org.keycloak.testframework.ui.annotations.InjectPage;
+import org.keycloak.testframework.ui.annotations.InjectWebDriver;
+import org.keycloak.testframework.ui.page.LoginPage;
+import org.openqa.selenium.WebDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.keycloak.tests.admin.model.workflow.WorkflowManagementTest.findEmailByRecipient;
+
+@KeycloakIntegrationTest(config = WorkflowsServerConfig.class)
+public class UserCreationTimeWorkflowTest {
+
+    private static final String REALM_NAME = "default";
+
+    @InjectRunOnServer(permittedPackages = "org.keycloak.tests")
+    RunOnServerClient runOnServer;
+
+    @InjectRealm
+    ManagedRealm managedRealm;
+
+    @InjectWebDriver
+    WebDriver driver;
+
+    @InjectPage
+    LoginPage loginPage;
+
+    @InjectOAuthClient
+    OAuthClient oauth;
+
+    @InjectMailServer
+    private MailServer mailServer;
+
+    @Test
+    public void testDisableUserBasedOnCreationDate() {
+        managedRealm.admin().workflows().create(WorkflowRepresentation.create()
+                .of(UserCreationTimeWorkflowProviderFactory.ID)
+                .withSteps(
+                        WorkflowStepRepresentation.create().of(NotifyUserStepProviderFactory.ID)
+                                .after(Duration.ofDays(5))
+                                .build(),
+                        WorkflowStepRepresentation.create().of(DisableUserStepProviderFactory.ID)
+                                .after(Duration.ofDays(5))
+                                .build()
+                ).build()).close();
+
+        // create a new user - this will trigger the association with the workflow
+        managedRealm.admin().users().create(
+                this.getUserRepresentation("alice", "Alice", "Wonderland", "alice@wornderland.org")).close();
+
+        // test running the scheduled steps
+        runOnServer.run((session -> {
+            RealmModel realm = configureSessionContext(session);
+            WorkflowsManager manager = new WorkflowsManager(session);
+
+            UserModel user = session.users().getUserByUsername(realm, "alice");
+            assertTrue(user.isEnabled());
+            assertNull(user.getAttributes().get("message"));
+
+            // running the scheduled tasks now shouldn't pick up any step as none are due to run yet
+            manager.runScheduledSteps();
+            user = session.users().getUserByUsername(realm, "alice");
+            assertTrue(user.isEnabled());
+            assertNull(user.getAttributes().get("message"));
+
+            try {
+                // set offset to 7 days - notify step should run now
+                Time.setOffset(Math.toIntExact(Duration.ofDays(6).toSeconds()));
+                manager.runScheduledSteps();
+                user = session.users().getUserByUsername(realm, "alice");
+                assertTrue(user.isEnabled());
+            } finally {
+                Time.setOffset(0);
+            }
+        }));
+
+        // Verify that the notify step was executed by checking email was sent
+        MimeMessage testUserMessage = findEmailByRecipient(mailServer, "alice@wornderland.org");
+        assertNotNull(testUserMessage, "The first step (notify) should have sent an email.");
+
+        mailServer.runCleanup();
+
+        // logging-in with alice should not reset the workflow - we should still run the disable step next
+        oauth.openLoginForm();
+        loginPage.fillLogin("alice", "alice");
+        loginPage.submit();
+        assertTrue(driver.getPageSource().contains("Happy days"));
+
+        // test running the scheduled steps
+        runOnServer.run((session -> {
+            RealmModel realm = configureSessionContext(session);
+            WorkflowsManager manager = new WorkflowsManager(session);
+
+            try {
+                // set offset to 11 days - disable step should run now
+                Time.setOffset(Math.toIntExact(Duration.ofDays(12).toSeconds()));
+                manager.runScheduledSteps();
+                UserModel user = session.users().getUserByUsername(realm, "alice");
+                assertFalse(user.isEnabled());
+            } finally {
+                Time.setOffset(0);
+            }
+        }));
+    }
+
+    private static RealmModel configureSessionContext(KeycloakSession session) {
+        RealmModel realm = session.realms().getRealmByName(REALM_NAME);
+        session.getContext().setRealm(realm);
+        return realm;
+    }
+
+    private UserRepresentation getUserRepresentation(String username, String firstName, String lastName, String email) {
+        UserRepresentation representation = new UserRepresentation();
+        representation.setUsername(username);
+        representation.setFirstName(firstName);
+        representation.setLastName(lastName);
+        representation.setEmail(email);
+        representation.setEnabled(true);
+        CredentialRepresentation credential = new CredentialRepresentation();
+        credential.setType(CredentialRepresentation.PASSWORD);
+        credential.setValue(username);
+        representation.setCredentials(List.of(credential));
+        return representation;
+    }
+}

tests/base/src/test/java/org/keycloak/tests/admin/model/workflow/WorkflowsScheduledTaskServerConfig.java
@@ -1,0 +1,13 @@
+package org.keycloak.tests.admin.model.workflow;
+
+import org.keycloak.models.workflow.WorkflowsEventListenerFactory;
+import org.keycloak.testframework.server.KeycloakServerConfigBuilder;
+
+public class WorkflowsScheduledTaskServerConfig extends WorkflowsServerConfig {
+
+    @Override
+    public KeycloakServerConfigBuilder configure(KeycloakServerConfigBuilder config) {
+        return super.configure(config)
+                .option("spi-events-listener--" + WorkflowsEventListenerFactory.ID + "--step-runner-task-interval", "1000");
+    }
+}